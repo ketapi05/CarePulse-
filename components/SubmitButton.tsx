8c21a4d76ac240161a8386868f5f94327de2ebdf
ketapi05 <ktnsisodiya05@gmail.com>
2024-07-12 18:10:54 +0530

    homepage-form-created

---
+++ components/SubmitButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
+
+interface ButtonProps {
+    isloading: boolean,
+    className: string,
+    children: React.ReactNode,
+}
+const SubmitButton = ({ isloading, className, children} : ButtonProps) => {
+  return (
+   <Button type='submit' disabled={isloading} className={className ?? 'shad-primary-btn w-full'}>
+     {isloading ? (
+        <div className='flex items-center gap-4'>
+            <Image
+              src="/assets/icons/loader.svg"
+              alt="loader"
+              width={24}
+              height={24}
+              className='animate-spin'
+            />
+            Loading....
+        </div>
+
+     ): children}
+   </Button>
+  )
+}
+
+export default SubmitButton
